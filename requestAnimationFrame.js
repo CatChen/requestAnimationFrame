3ea889eff8695bfa090dc43e9daf9fb31eef6206
Cat Chen <cathsfz@gmail.com>
2012-01-05 17:02:42 +0800

    added visibility api support

--- requestAnimationFrame.js
+++ requestAnimationFrame.js
@@ -9,9 +9,16 @@
     }
     
     if (!('requestAnimationFrame' in window )) {
+        var STOP = 0;
+        var START = 1;
+        var PAUSE = -1;
+        var INTERVAL = 1000;
+        
         var nextHandle = 1;
-        var frameQueue = [];
-        var timerEnabled = false;
+        var inQueue = [];
+        var outQueue;
+        var timerState = STOP;
+        var timerHandle;
         
         var enqueue = function(callback) {
             var handle = nextHandle++;
@@ -19,12 +26,30 @@
                 handle: handle,
                 callback: callback
             };
-            frameQueue.push(bundle);
+            inQueue.push(bundle);
             return handle;
         };
         
+        var deleteFromQueue = function(handle) {
+            for (var i = 0, l = inQueue.length; i < l; i++) {
+                if (inQueue[i].handle === handle) {
+                    inQueue.splice(i, 1);
+                    return;
+                }
+            }
+        };
+        
+        var clearQueue = function() {
+            inQueue.length = 0;
+        };
+        
+        var switchQueue = function() {
+            outQueue = inQueue;
+            inQueue = [];
+        };
+        
         var dequeue = function() {
-            var bundle = frameQueue.shift();
+            var bundle = outQueue.shift();
             if (bundle) {
                 return bundle.callback;
             } else {
@@ -32,18 +57,11 @@
             }
         };
         
-        var deleteFromQueue = function(handle) {
-            for (var i = 0, l = frameQueue.length; i < l; i++) {
-                if (frameQueue[i].handle === handle) {
-                    frameQueue.splice(i, 1);
-                    return;
-                }
-            }
-        };
-        
         var timer = function() {
-            while (frameQueue.length > 0) {
-                var callback = dequeue();
+            var callback;
+            switchQueue();
+            stopTimer();
+            while (callback = dequeue()) {
                 if (callback) {
                     try {
                         callback(+new Date());
@@ -52,23 +70,54 @@
                     }
                 }
             };
-            disableTimer();
         };
         
-        var enableTimer = function() {
-            if (!timerEnabled && frameQueue.length > 0) {
-                timerEnabled = true;
-                setTimeout(timer, 1000 / 60);
+        var startTimer = function() {
+            if (timerState === STOP && inQueue.length > 0) {
+                timerState = START;
+                timerHandle = setTimeout(timer, INTERVAL);
             }
         };
         
-        var disableTimer = function() {
-            timerEnabled = false;
+        var stopTimer = function() {
+            if (timerState === START) {
+                timerState = STOP;
+                clearTimeout(timerHandle);
+                clearQueue();
+            }
         };
+        
+        var pauseTimer = function() {
+            if (timerState === START) {
+                timerState = PAUSE;
+                clearTimeout(timerHandle);
+            }
+        };
+        
+        var resumeTimer = function() {
+            if (timerState === PAUSE) {
+                timerState = START;
+                timerHandle = setTimeout(timer, INTERVAL);
+            }
+        };
+        
+        for (var i = 0, l = prefixes.length; i < l; i++) {
+            if ((prefixes[i] + 'Hidden') in document) {
+                (function(prefix) {
+                    document.addEventListener(prefix + 'visibilitychange', function() {
+                        if (document[prefix + 'Hidden']) {
+                            pauseTimer();
+                        } else {
+                            resumeTimer();
+                        }
+                    });
+                })(prefixes[i]);
+            }
+        }
         
         window.requestAnimationFrame = function(callback) {
             var handle = enqueue(callback);
-            enableTimer();
+            startTimer();
             return handle;
         };
         
